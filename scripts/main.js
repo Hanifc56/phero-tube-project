9c47eb88c7728a7e803824e4dae40054c1ab13a5
Hanifc56 <abuhanif007sd@gmail.com>
2023-10-18 23:55:18 +0600

    done for me

--- scripts/main.js
+++ scripts/main.js
@@ -10,13 +10,16 @@ const handleCatagory = async () => {
         <a onclick="getVideos('${category.category_id}')" class="tab bg-gray-400 text-black rounded-lg m-2 text-xl hover:bg-red-500 hover:text-white">${category.category}</a> 
         `;
         tabContainer.appendChild(tabDiv);
-    }) 
+    })
+    // my_modal_1.showModal(); 
+    
 };
 const getVideos = async (category_id)=>{
     const response = await fetch(`https://openapi.programming-hero.com/api/videos/category/${category_id}`);
     const data = await response.json();
     const cardContainer = document.getElementById('card-container');
     cardContainer.innerHTML="";
+    onClickBlog('1005');
     data.data.forEach((videoCard)=>{
         console.log(videoCard);
         const div = document.createElement('div');
@@ -49,8 +52,32 @@ const getVideos = async (category_id)=>{
         `;
         cardContainer.appendChild(div);
     })
+    // my_modal_1.showModal();
+    
 }
 
+const onClickBlog=() =>{
+  const modalContainer = document.getElementById('modal-container');
+  const btn = document.createElement('div');
+   btn.innerHTML=`
+   
+   <div class=" flex justify-center"><img src="images/Icon.png" alt="">
+   <h1>NO VIDEO FOUND!</h1></div>
+   <dialog id="my_modal_1" class="modal">
+     <div class="modal-box">
+       <h3 class="font-bold text-lg">Hello!</h3>
+       <p class="py-4">Press ESC key or click the button below to close</p>
+       <div class="modal-action">
+         <form method="dialog">
+           <!-- if there is a button in form, it will close the modal -->
+           <button class="btn">Close</button>
+         </form>
+       </div>
+     </div>
+   </dialog>
+  `;
+  modalContainer.appendChild(btn);
+}
 
 
 
