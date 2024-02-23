831cc8de876f8dc32fb02c1d1aae697663faa745
Hanifc56 <abuhanif007sd@gmail.com>
2024-02-23 22:03:56 +0600

    readme.md file added

---
+++ readMe.md
@@ -0,0 +1,21 @@
+# This is 6th Assignment of my course
+
+# key features of this site
+
+<!-- ? -->
+
+1. This site can filter videos by its category
+<!-- ? -->
+2. This site can show if there is no content
+<!-- ?  -->
+3. There is a short by view to see videos according to views
+<!-- ? -->
+4. There is a blog category for blogs
+<!-- ? -->
+5. This is a staric site that is similar to Youtube
+
+# Live site : https://ptube-site.netlify.app/
+
+ <!-- ! -->
+
+# Thanks for Visiting!!
--- scripts/main.js
+++ scripts/main.js
@@ -1,48 +1,53 @@
-const handleCatagory = async () => { 
-    const response=await fetch('https://openapi.programming-hero.com/api/videos/categories');
-    const data = await response.json();
-    const tabContainer =document.getElementById('tab-container');
-    const dataChain = data.data;
-    dataChain.forEach((category)=>{
-        const tabDiv = document.createElement('div');
-        // tabDiv.classList.add('');
-        tabDiv.innerHTML=`
+const handleCatagory = async () => {
+  const response = await fetch(
+    "https://openapi.programming-hero.com/api/videos/categories"
+  );
+  const data = await response.json();
+  const tabContainer = document.getElementById("tab-container");
+  const dataChain = data.data;
+  dataChain.forEach((category) => {
+    const tabDiv = document.createElement("div");
+    // tabDiv.classList.add('');
+    tabDiv.innerHTML = `
         <a onclick="getVideos('${category.category_id}')" class="tab bg-gray-400 text-black rounded-lg m-2 text-xl hover:bg-red-500 hover:text-white">${category.category}</a> 
         `;
-        tabContainer.appendChild(tabDiv);
-    })
-   
-    
+    tabContainer.appendChild(tabDiv);
+  });
 };
-const getVideos = async (category_id)=>{
-    const response = await fetch(`https://openapi.programming-hero.com/api/videos/category/${category_id}`);
-    const data = await response.json();
-    const cardContainer = document.getElementById('card-container');
-    cardContainer.innerHTML="";
-  
-    data.data.forEach((videoCard)=>{
-        console.log(videoCard);
-        const div = document.createElement('div');
-        div.classList.add('grid');
-        div.innerHTML=`
+const getVideos = async (category_id) => {
+  const response = await fetch(
+    `https://openapi.programming-hero.com/api/videos/category/${category_id}`
+  );
+  const data = await response.json();
+  const cardContainer = document.getElementById("card-container");
+  cardContainer.innerHTML = "";
+  // use conditon to show the data if there is no data then show the no vidios
+  data.data.forEach((videoCard) => {
+    console.log(videoCard);
+    const div = document.createElement("div");
+    div.classList.add("grid");
+    div.innerHTML = `
         <div class="">
         <div class="card w-96 h-96">
-          <figure class="w-full rounded-xl"><img src=${videoCard.thumbnail} alt="" /></figure>
+          <figure class="w-full rounded-xl"><img src=${
+            videoCard.thumbnail
+          } alt="" /></figure>
           <div class="flex py-8 px-4">
               <div class="avatar ">
                 <div class="w-12 h-12 rounded-full">
-                  <img src=${videoCard?.authors[0].profile_picture
-                  } />
+                  <img src=${videoCard?.authors[0].profile_picture} />
                   
                 </div>
               </div>
               <div class="pl-8">
                   <h2 class="card-title">
-                    ${videoCard.title
-                    }
+                    ${videoCard.title}
                   </h2>
-                 <p>${videoCard?.authors[0].profile_name
-                 } <span>${videoCard.authors[0].verified ? '<img src="images/verified.svg"></img>' : ''} </span></p>
+                 <p>${videoCard?.authors[0].profile_name} <span>${
+      videoCard.authors[0].verified
+        ? '<img src="images/verified.svg"></img>'
+        : ""
+    } </span></p>
                
                  <p>${videoCard.others.views} views</p>
               </div>
@@ -50,16 +55,15 @@ const getVideos = async (category_id)=>{
         </div>
       </div>
         `;
-        cardContainer.appendChild(div);
-    })
-    onClickBlog('1005');
-    
-}
+    cardContainer.appendChild(div);
+  });
+  onClickBlog("1005");
+};
 
-const onClickBlog=() =>{
-  const modalContainer = document.getElementById('modal-container');
-  const btn = document.createElement('div');
-   btn.innerHTML=`
+const onClickBlog = () => {
+  const modalContainer = document.getElementById("modal-container");
+  const btn = document.createElement("div");
+  btn.innerHTML = `
    
    <div class=" flex justify-center"><img src="images/Icon.png" alt="">
    <h1>NO VIDEO FOUND!</h1></div>
@@ -77,14 +81,7 @@ const onClickBlog=() =>{
    </dialog>
   `;
   modalContainer.appendChild(btn);
-}
-
-
-
-
-
-
-
+};
 
 handleCatagory();
-getVideos('1000', '1001','1002', '1003');
+getVideos("1000", "1001", "1002", "1003");
